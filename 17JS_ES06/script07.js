32a84d1a61617a05f9fc8d94d505eda0f7478faa
unknown <yongpro0526@gmail.com>
2025-12-05 17:38:50 +0900

    study

---
+++ 17JS_ES06/script07.js
@@ -0,0 +1,8 @@
+// 모듈 익스포트
+// 모듈 - 미리 작성해 놓은 JS파일
+// 모듈안에는 클래스, 함수, 변수, 인터페이스 등이 정의되어있음.
+
+// 모듈을 내보낼때는 export 키워드를 사용
+export const name = "홍길동";
+export const age = 20;
+export const getInfo = () => {}
---
+++ 17JS_ES06/script08.js
@@ -0,0 +1,12 @@
+// default export 구문
+// 단 하나의 클래스 or 함수 등을 내보내는 방식
+
+const name = "홍길동";
+const age = 20;
+const hobby = [];
+
+function Person() {
+
+}
+
+export default Person;
